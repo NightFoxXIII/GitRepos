d8d74a3c87c9b1fc774b89d3b5738f26326061b3
neonium <ericdvachon@gmail.com>
2014-03-23 19:17:53 -0600

    Sync

---
+++ CPSC481AirHifi(GitHub)/CPSC481AirHifi(GitHub)/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPSC481AirHifi_GitHub_
+{
+    class Data
+    {
+        private string HotelArtsReviews = "fdsfsdf";
+        private string HotelBlueReviews = "dsfsfsd";
+        private string ThePurpleHotelReviews = "";
+    }
+}
